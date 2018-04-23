5a0699447facd8a8f94aeb69053ebbe88818e708
ChrstnDmbchr <christiandambacher@gmail.com>
2018-04-23 18:38:20 +0100

    bot now joining correct channel, mp3 currently not playing

--- soundboard/soundboard.js
+++ soundboard/soundboard.js
@@ -8,7 +8,30 @@ let connection = null;
 let dispatcher = null;
 
 module.exports.run = async (bot, message, args) => {
- console.log(args);
+  
+  // Enter the voice channel of user ItsChrstn as soundboard does not message
+  const guildArr = Array.from(bot.guilds)
+  let guildMember
+  for (let i = 0; i < guildArr.length; i++) {
+    if(guildArr[i][1].members.get('136499610947551232').voiceChannelID){
+      guildMember = guildArr[i][1].members.get('136499610947551232')
+    }
+  };
+  //
+
+  if (guildMember) {
+    if (!connection) {
+      connection = await guildMember.voiceChannel.join();
+    }
+    dispatcher = connection.playFile(`./audio/${args.play}.mp3`);
+    dispatcher.setVolume(0.5);
+    dispatcher.on('end', end => {
+      guildMember.voiceChannel.leave();
+    })
+  } else {
+    console.log('ERROR: User ItsChrstn is not in a voice channel, soundboard not playing.')
+  }
+
 };
 
 module.exports.help = {
